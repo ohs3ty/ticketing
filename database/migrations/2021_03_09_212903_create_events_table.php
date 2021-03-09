c1fd45889c5a8d15ffc7bba82355112a04c1b01a
Caleb <csly2@byu.edu>
2021-03-09 14:41:21 -0700

    events migration

--- database/migrations/2021_03_09_212903_create_events_table.php
+++ database/migrations/2021_03_09_212903_create_events_table.php
@@ -16,6 +16,18 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('event_name');
+            $table->text('event_description')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->integer('venue_id')->unsigned();
+            $table->integer('event_type_id')->unsigned();
+            $table->integer('organization_id')->unsigned();
+            $table->foreign('venue_id')->references('id')->on('venues');
+            $table->foreign('event_type_id')->references('id')->on('event_types');
+            $table->foreign('organization_id')->references('id')->on('organizations');
         });
     }
 
