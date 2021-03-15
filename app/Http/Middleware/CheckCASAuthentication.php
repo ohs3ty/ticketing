deb53c05bfc5a7607cf16735505bb393a71c8cd7
Caleb <csly2@byu.edu>
2021-03-15 14:43:48 -0600

    change cas page

--- app/Http/Middleware/CheckCASAuthentication.php
+++ app/Http/Middleware/CheckCASAuthentication.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\People\User;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,32 +18,23 @@ class CheckCASAuthentication
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check())
+        if( Auth::check() )
             return $this->catch_cas($next($request));
 
-        if (config('app.debug') == TRUE && config('app.env') == 'local') {
-            Auth::login(User::generateByCAS(
-                [
-                    'user' => 'testset',
-                    'attributes' => [
-                        'memberOf' => User::PROGRAMMER,
-                    ]
-                ]
-            ));
-        } else {
-            phpCAS::client(CAS_VERSION_2_0, 'cas.byu.edu', 443, 'cas');
-            phpCAS::setNoCasServerValidation();
-            phpCAS::forceAuthentication();
-            Auth::login(User::generateByCAS($_SESSION['phpCAS']));
-        }
+        phpCAS::client(CAS_VERSION_2_0,'cas.byu.edu',443,'cas');
+        phpCAS::setNoCasServerValidation();
+        phpCAS::forceAuthentication();
+        Auth::login(\App\User::findUser($_SESSION['phpCAS']));
+
         return $this->catch_cas($next($request));
     }
 
     private function catch_cas($n)
     {
-        if (get_class($n) == 'Illuminate\Http\RedirectResponse') {
+        if(get_class($n) == "Illuminate\Http\RedirectResponse")
+        {
             $url = ($n->getTargetURL());
-            if (strpos($url, 'cas.byu.edu') !== FALSE)
+            if(strpos($url, "cas.byu.edu") !== FALSE)
                 return redirect('');
         }
         return $n;
