e3f58f37478c39baf5bae12f0564cc92472bad6e
Caleb <csly2@byu.edu>
2021-03-16 14:19:24 -0600

    debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,9 +56,9 @@
         </byu-menu>
         <byu-user-info slot="user">
             @guest <!--only guest-->
-                <a slot='login' href="/cas-login">Sign In</a>
+                <a slot='login' href="/cas-login">Not Sign In</a>
                 <a slot="logout" href="{{ route('logout') }}"></a>
-                Still not logged in
+
             @else <!-- Logged in -->
                 <span slot='user-name'> {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</span>
 
