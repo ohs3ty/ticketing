1d66b6f80ffb6f18826fee5970aac8e1c4d25c70
Caleb <csly2@byu.edu>
2021-03-16 14:43:13 -0600

    debug

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function findUser($phpCAS) {
+        print($phpCAS);
+    }
 }
