760bcbd3d2c3bf84ec0df5c3b355044d04ab6e28
Caleb <csly2@byu.edu>
2021-03-29 15:42:30 -0600

    debug logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,6 +42,7 @@ class LoginController extends Controller
 
     public function logout(Request $request)
     {
+        dd("works");
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
