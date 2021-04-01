53867afbfbc724360e4cabf10f26e5f43b38bc75
Caleb <csly2@byu.edu>
2021-04-01 14:38:33 -0600

    debug edit ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -93,7 +93,7 @@ class TicketController extends Controller
                                     ->where('ticket_types.id', $ticket_type_id)
                                     ->first();
 
-        dd($ticket_type);
+        dd($ticket_type->ticket_limit);
 
                                     // return ($ticket_type);
         return view('ticket.edit_ticket', [
