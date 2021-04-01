2d19c160b930940808c442c0212920e854af9043
Caleb <csly2@byu.edu>
2021-04-01 14:50:01 -0600

    debug ticket_limit

--- resources/views/ticket/edit_ticket.blade.php
+++ resources/views/ticket/edit_ticket.blade.php
@@ -100,7 +100,7 @@ Add Ticket Type
 
         if (document.getElementById("unlimited").checked == true) {
             document.getElementById("ticket_limit").disabled = true;
-            document.getElementById("ticket_limit").value = 0;
+            document.getElementById("ticket_limit").innerHTML = 0;
         }
     })
 </script>
