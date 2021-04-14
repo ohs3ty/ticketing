108e7f081f13a2fc832da53bc69f9dd1a51d2cb2
Caleb <csly2@byu.edu>
2021-04-14 13:51:43 -0600

    testing modal

--- resources/views/event/event_index.blade.php
+++ resources/views/event/event_index.blade.php
@@ -14,7 +14,7 @@ View All Events
         <section>
         @foreach ($events as $event)
             @if (\Carbon\Carbon::parse($event->start_date)->format('n') == ($i + 1))
-                <div onclick="location.href = '{{route('eventdetail', ['id' => $event->id])}}'" class="eventDetails card" style="border-color: lightgrey">
+                <div class="eventDetails card" style="border-color: lightgrey">
                     <div class="row no-gutters">
                         <div class="col-sm-2">
                             <div class="card-body text-right">
@@ -37,5 +37,29 @@ View All Events
         @endforeach
             </section>
     @endfor
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
