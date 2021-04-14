f83b0787b9618733ddcf0c362f71da18ff4e7a67
Caleb <csly2@byu.edu>
2021-04-14 13:48:36 -0600

    try with event

--- resources/views/event/event_index.blade.php
+++ resources/views/event/event_index.blade.php
@@ -14,7 +14,7 @@ View All Events
         <section>
         @foreach ($events as $event)
             @if (\Carbon\Carbon::parse($event->start_date)->format('n') == ($i + 1))
-                <div onclick="location.href = '{{url('eventdetail')}}'" class="eventDetails card" style="border-color: lightgrey">
+                <div onclick="location.href = '{{route('eventdetail', ['id' => $event->id])}}'" class="eventDetails card" style="border-color: lightgrey">
                     <div class="row no-gutters">
                         <div class="col-sm-2">
                             <div class="card-body text-right">
