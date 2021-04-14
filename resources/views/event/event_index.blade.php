9b5e7c38b86af742ef263d4051db26c10214de17
Caleb <csly2@byu.edu>
2021-04-14 14:00:11 -0600

    test modal

--- resources/views/event/event_index.blade.php
+++ resources/views/event/event_index.blade.php
@@ -31,8 +31,29 @@ View All Events
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <button class="btn" data-toggle="modal" data-target="#{{$event->event_name}}{{$event->event_id}}">View Details</button>
+                            {{-- modal button --}}
+                            <button class="btn" data-toggle="modal" data-target="#event{{$event->event_id}}">View Details</button>
                         </div>
+                        <!-- Modal -->
+                            <div id="event{{$event->event_id}}" class="modal fade" role="dialog">
+                                <div class="modal-dialog">
+
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <p>Some text in the modal.</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+
+                                </div>
+                            </div>
+                        {{-- End of modal --}}
                     </div>
                 </div>
                 <br>
@@ -40,28 +61,4 @@ View All Events
         @endforeach
             </section>
     @endfor
-
-<!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-
 @endsection
