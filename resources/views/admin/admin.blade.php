a9948593bddd0f1079a5fafb21959cc3ab04ca78
Caleb <csly2@byu.edu>
2021-04-14 15:42:09 -0600

    admin page

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -4,24 +4,27 @@
     Admin
 @endsection
 @section('content')
-<h3>Organizations</h3>
-<div class="container">
-    <a class="btn btn-primary" href="{{ url('/admin/addorganization') }}" style="border-color: lightgrey">Add Organization</a>
-</div>
-<br>
-<div class="container">
-    <div class="row">
-          @foreach($organizations as $organization)
-        <div class="col-4 text-center" style="margin-bottom: 15px; border-color: rgb(179, 179, 179);" >
-            <div class="card">
-              <div class="card-body">
-                <a href="{{ route('/admin/organization', ['name' => $organization->organization_name]) }}" class="card-title" style="color: black; font-size: large;">{{ $organization->organization_name }}</a>
-              </div>
-            </div>
-          </div>
-        @endforeach
+
+@if(Auth::user()->role)
+    <h3>Organizations</h3>
+    <div class="container">
+        <a class="btn btn-primary" href="{{ url('/admin/addorganization') }}" style="border-color: lightgrey">Add Organization</a>
     </div>
-</div>
+    <br>
+    <div class="container">
+        <div class="row">
+            @foreach($organizations as $organization)
+            <div class="col-4 text-center" style="margin-bottom: 15px; border-color: rgb(179, 179, 179);" >
+                <div class="card">
+                <div class="card-body">
+                    <a href="{{ route('/admin/organization', ['name' => $organization->organization_name]) }}" class="card-title" style="color: black; font-size: large;">{{ $organization->organization_name }}</a>
+                </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+@endif
 
 {{Auth::user()->role}}
 
