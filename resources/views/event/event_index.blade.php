0f6307caf368d1695334ff7374d4b8273d727bea
Caleb <csly2@byu.edu>
2021-04-14 14:04:32 -0600

    test

--- resources/views/event/event_index.blade.php
+++ resources/views/event/event_index.blade.php
@@ -31,7 +31,7 @@ View All Events
                             </div>
                         </div>
                         <div class="col-sm-2">
-                            <div class="text-center" style="padding-top: 10%;">
+                            <div class="text-center" style="padding-top: 20%;">
                                 {{-- modal button --}}
                                 <button class="btn" data-toggle="modal" data-target="#event{{$event->event_id}}">View Details</button>
                             </div>
