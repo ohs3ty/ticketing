7c5532d51003e243573ee84e82aeb1a895034969
Caleb <csly2@byu.edu>
2021-04-14 14:14:57 -0600

    adding details

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -4,13 +4,15 @@
     <!-- Modal content-->
     <div class="modal-content">
         <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-        <p>Some text in the modal.</p>
+            Event name: {{ $event->event_name }}
+            Start Date:
+
         </div>
         <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
     </div>
 
