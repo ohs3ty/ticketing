eb085f98516cac80b1f79741f2768321e1866a45
Caleb <csly2@byu.edu>
2021-04-15 10:52:45 -0600

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,40 +11,37 @@ Home
 @section('content')
 <div class="container">
     <h3>This Week's Events</h3>
-    <br>
 
     @foreach ($events as $event)
-            @if (\Carbon\Carbon::parse($event->start_date)->format('n') == ($i + 1))
-                <div class="card" style="border-color: lightgrey">
-                    <div class="row no-gutters">
-                        <div class="col-sm-2">
-                            <div class="card-body text-right">
-                                <h3 style="margin-bottom: 0px;">{{ $months[$i]}}</h3>
-                                <h2 style="margin: none">{{ \Carbon\Carbon::parse($event->start_date)->format('j') }}</h2>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-body">
-                                <h3 class="card-title">{{ $event->event_name }}</h3>
-                                <h5 class="card-subtitle text-muted">{{ \Carbon\Carbon::parse($event->start_date)->format('F j, Y') }}
-                                    at {{ \Carbon\Carbon::parse($event->start_date)->format('g:i a') }}</h5>
-                                <p style="margin-top: 10px;" class="card-text">{{ $event->event_description }}</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-2">
-                            <div class="text-center" style="padding-top: 25%;">
-                                {{-- modal button --}}
-                                <button class="btn" data-toggle="modal" data-target="#event{{$event->event_id}}">View Details</button>
-                            </div>
-                        </div>
-                        <!-- Modal -->
-                            @include('event.event_modal.event_detail')
-                        {{-- End of modal --}}
+        <div class="card" style="border-color: lightgrey">
+            <div class="row no-gutters">
+                <div class="col-sm-2">
+                    <div class="card-body text-right">
+                        <h3 style="margin-bottom: 0px;">{{ \Carbon\Carbon::parse($event->start_date)->format('F') }}</h3>
+                        <h2 style="margin: none">{{ \Carbon\Carbon::parse($event->start_date)->format('j') }}</h2>
                     </div>
                 </div>
-                <br>
-            @endif
-        @endforeach
+                <div class="col-sm-8">
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $event->event_name }}</h3>
+                        <h5 class="card-subtitle text-muted">{{ \Carbon\Carbon::parse($event->start_date)->format('F j, Y') }}
+                            at {{ \Carbon\Carbon::parse($event->start_date)->format('g:i a') }}</h5>
+                        <p class="card-text">{{ $event->event_description }}</p>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="text-center" style="padding-top: 25%;">
+                        {{-- modal button --}}
+                        <button class="btn" data-toggle="modal" data-target="#event{{$event->event_id}}">View Details</button>
+                    </div>
+                </div>
+                <!-- Modal -->
+                    @include('event.event_modal.event_detail')
+                {{-- End of modal --}}
+            </div>
+        </div>
+        <br>
+    @endforeach
     @guest
         Guest only
 
