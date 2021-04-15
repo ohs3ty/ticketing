55abcbe547fca2960c8acac0bf2977f5dd7b8b29
Caleb <csly2@byu.edu>
2021-04-15 11:38:58 -0600

    adding ticket

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,12 +6,19 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\EventTimes;
+use App\Models\TicketType;
 use DateInterval;
 use DateTime;
 
 class TransactionController extends Controller {
     public function buy_ticket(Request $request) {
         print($request->event_id);
-        return view('transaction.buy_ticket');
+
+        $ticket_types = TicketType::where('event_id' == $request->event_id)
+                        ->get();
+
+        return view('transaction.buy_ticket', [
+            "ticket_types" => $ticket_types,
+        ]);
     }
 }
