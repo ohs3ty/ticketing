f425bfb8e7f996f82f761a59c9a3c297a75f7605
Caleb <csly2@byu.edu>
2021-04-15 12:52:07 -0600

    attempt to see if ticket works

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -12,8 +12,12 @@ use DateTime;
 
 class TransactionController extends Controller {
     public function buy_ticket(Request $request) {
-        $ticket_types = TicketType::where('event_id', $request->event_id)
-                        ->get();
+        $ticket_types = TicketType::select("ticket_types.id", "ticket_name", "ticket_cost", "ticket_limit", "ticket_description", "event_id", "patron_profile_id", "ticket_open_date", "ticket_close_date",
+                                        DB::raw('COUNT(ticket_types.id) AS ticket_type_count')
+                            ->where('event_id', $request->event_id)
+                            ->leftJoin('transaction_tickets', 'transaction_tickets.ticket_type_id', '=', 'ticket_types.id')
+                            ->groupBy("ticket_types.id", "ticket_name", "ticket_cost", "ticket_limit", "ticket_description", "event_id", "patron_profile_id", "ticket_open_date", "ticket_close_date")
+                            ->get();
 
         return view('transaction.buy_ticket', [
             "ticket_types" => $ticket_types,
