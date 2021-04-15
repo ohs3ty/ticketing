bbaa14aaf95fad489be620e6edefc26d619a6faf
Caleb <csly2@byu.edu>
2021-04-15 12:55:08 -0600

    fix on tickets page

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\EventTimes;
 use App\Models\TicketType;
+use Illuminate\Support\Facades\DB;
 use DateInterval;
 use DateTime;
 
 class TransactionController extends Controller {
     public function buy_ticket(Request $request) {
         $ticket_types = TicketType::select("ticket_types.id", "ticket_name", "ticket_cost", "ticket_limit", "ticket_description", "event_id", "patron_profile_id", "ticket_open_date", "ticket_close_date",
-                                        DB::raw('COUNT(ticket_types.id) AS ticket_type_count')
+                                        DB::raw('COUNT(ticket_types.id) AS ticket_type_count'))
                             ->where('event_id', $request->event_id)
                             ->leftJoin('transaction_tickets', 'transaction_tickets.ticket_type_id', '=', 'ticket_types.id')
                             ->groupBy("ticket_types.id", "ticket_name", "ticket_cost", "ticket_limit", "ticket_description", "event_id", "patron_profile_id", "ticket_open_date", "ticket_close_date")
