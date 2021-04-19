2bf609e769b23b81908ccb824a665b7ee009eb1f
Caleb <csly2@byu.edu>
2021-04-19 13:58:27 -0600

    test

--- resources/views/transaction/buy_ticket.blade.php
+++ resources/views/transaction/buy_ticket.blade.php
@@ -7,12 +7,15 @@
 @section('content')
     <div class="row">
         @foreach ( $ticket_types as $ticket_type)
-        <div class="col-3 bg-primary">
+        <div class="col-3">
             <div class="card mb-3" style="max-width: 18rem;">
                 {{-- <div class="card-header"></div> --}}
                 <div class="card-body">
                     <h5 class="card-title">{{ $ticket_type->ticket_name }}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">
+                        Ticket Description
+                        {{$ticket_type}}
+                    </p>
                 </div>
             </div>
         </div>
