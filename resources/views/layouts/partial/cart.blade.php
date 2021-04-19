4ccbb626a7039f40e368a574984a51b3b9126836
Caleb <csly2@byu.edu>
2021-04-19 14:58:37 -0600

    test

--- resources/views/layouts/partial/cart.blade.php
+++ resources/views/layouts/partial/cart.blade.php
@@ -1,3 +1,3 @@
-<div class="text-right" style="font-size: 2rem; margin-right: 10px;">
+<div class="text-right" style="font-size: 1.5rem; margin-right: 10px;">
     <i class="bi bi-cart"></i>
 </div>
