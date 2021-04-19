cd684b87c0dbfb062dedd116ad087627575939d6
Caleb <csly2@byu.edu>
2021-04-19 13:48:25 -0600

    debug

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -34,7 +34,6 @@
 
                                     @endif
 
-                                    {{ $ticket_count }}
                                 @endforeach
                             </div>
                         @endif
