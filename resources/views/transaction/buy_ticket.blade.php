f2047ba038817b3adb0e92fc090eee7a0e5b8f0d
Caleb <csly2@byu.edu>
2021-04-19 14:20:43 -0600

    change formatting

--- resources/views/transaction/buy_ticket.blade.php
+++ resources/views/transaction/buy_ticket.blade.php
@@ -23,13 +23,13 @@
                                                 {{$ticket_type->ticket_description}}
                                             @endif<br>
                         Ticket Cost: ${{$ticket_type->ticket_cost}}<br>
+                    </p>
                         @if ((now() > $ticket_type->ticket_close_date) || (now() < $ticket_type->ticket_open_date))
                             <div class="text-center text-danger">
                                 No tickets can be bought at this time
                             </div>
                         @endif
-                    </p>
-                    {{$ticket_type}}
+                    {{ $ticket_type }}
 
                 </div>
             </div>
