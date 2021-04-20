37d56df4600b57454bc06216e1976744e12377bf
Caleb <csly2@byu.edu>
2021-04-20 12:49:34 -0600

    modal

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -11,8 +11,7 @@
             <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-            Start of Event: {{ \Carbon\Carbon::parse($event->start_date)->format('F j, Y, g:i a') }}<br>
-            End of Event: {{ \Carbon\Carbon::parse($event->end_date)->format('F j, Y, g:i a') }}<br>
+            Event Time: {{ \Carbon\Carbon::parse($event->start_date)->format('F j, Y, g:i a') }} to {{ \Carbon\Carbon::parse($event->end_date)->format('F j, Y, g:i a') }}<br>
             Description: {{ $event->event_description }}<br>
             Ticket Cost: ${{ $event->ticket_cost }}<br>
             Ticket Groups:<br>
