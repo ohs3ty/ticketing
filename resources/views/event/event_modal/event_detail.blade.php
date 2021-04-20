6d06da848a6c7a7297b5c87e436c2a92da2c1345
Caleb <csly2@byu.edu>
2021-04-20 14:50:37 -0600

    test

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -24,19 +24,22 @@
             @if($event->ticket_type_count > 0)
                 @foreach ($ticket_counts as $ticket_count)
                     @if(($ticket_count->id == $event->id))
-                        <div class="row container" style="padding: 10px">
-                            <div class="col-8">
-                                <h5>{{ $ticket_count->ticket_name }}</h5>
-                                <h5>${{ number_format($ticket_count->ticket_cost, 2, '.', ',') }}</h5>
-                            </div>
-                            <div class="col-4">
-                                <h5>Ticket Quantity</h5>
-                                @if ($ticket_count->ticket_left == null)
-                                    {{ Form::selectRange('number', 0, 100, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
-                                @else
-                                    {{ Form::selectRange('number', 0, $ticket_count->ticket_left, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
-                                @endif
-                            </div>
+                    {{-- if past or before sale date range --}}
+                        @if((now() >= $ticket_count->ticket_open_date) && (now() <= $ticket_count->ticket_close_date))
+                            <div class="row container" style="padding: 10px">
+                                <div class="col-8">
+                                    <h5>{{ $ticket_count->ticket_name }}</h5>
+                                    <h5>${{ number_format($ticket_count->ticket_cost, 2, '.', ',') }}</h5>
+                                </div>
+                                <div class="col-4">
+                                    <h5>Ticket Quantity</h5>
+                                    @if ($ticket_count->ticket_left == null)
+                                        {{ Form::selectRange('number', 0, 100, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
+                                    @else
+                                        {{ Form::selectRange('number', 0, $ticket_count->ticket_left, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
+                                    @endif
+                                </div>
+                            @endif
                         </div>
                         <hr>
                     @endif
@@ -50,7 +53,7 @@
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             {{-- buy tickets button --}}
             @if ($event->ticket_type_count > 0)
-                <a class="btn btn-default" href="{{ route('buy_ticket', ['event_id' => $event->id]) }}">Buy Tickets</a>
+                <a class="btn btn-default" href="{{ route('buy_ticket', ['event_id' => $event->id]) }}">Add to Cart</a>
             @endif
         </div>
     </div>
