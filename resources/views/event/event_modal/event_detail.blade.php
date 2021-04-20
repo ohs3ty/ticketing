fa5a24f49c8a30e78012f5c96e382165a579f237
Caleb <csly2@byu.edu>
2021-04-20 14:02:31 -0600

    test

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -44,41 +44,6 @@
                 <p>No tickets are currently available</p>
             @endif
             {{ $event }}
-
-            ticket
-
-
-            Ticket Groups:<br>
-            <div class="container">
-                @if($event->ticket_type_count > 0)
-                    @foreach ($ticket_types as $ticket_type)
-                        @if($ticket_type->event_id == $event->id)
-                            {{ $ticket_type->ticket_name }}<br>
-                            <div class="container">
-                                Ticket sales for this group starts on {{\Carbon\Carbon::parse($ticket_type->ticket_open_date)->format('F j, Y')}}
-                                and closes on (ticket close date)<br>
-                                @foreach ($ticket_counts as $ticket_count)
-
-                                    @if(($ticket_count->id == $event->id) && ($ticket_count->ticket_name == $ticket_type->ticket_name))
-                                    Number of Tickets Left:
-
-                                        @if ($ticket_count->ticket_limit == null)
-                                            Unlimited
-                                        @else
-                                            {{ $ticket_count->ticket_limit }}
-                                        @endif
-
-                                    @endif
-
-                                @endforeach
-                            </div>
-                        @endif
-                    @endforeach
-                @else
-                    No Tickets Available
-                @endif
-
-            </div>
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
