16e94c5890473e5cd83b6bb6bdafcdbcac42f8c8
Caleb <csly2@byu.edu>
2021-04-26 12:56:08 -0600

    test

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -27,7 +27,7 @@ class TransactionController extends Controller {
         print(count($request->ticket_quantity));
         foreach ($request->ticket_quantity as $i) {
             foreach($i as $j) {
-                print($j);
+                print($i->Student);
             }
         }
         dd($request->ticket_quantity);
