4b9d1fb54eb0e4780aefc28e203b09ce692fbb77
Caleb <csly2@byu.edu>
2021-04-26 15:52:45 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -19,7 +19,7 @@ View Cart
             </div>
             <div class="col-2">
                 <div class="input-group mb-3">
-                    {{ Form::selectRange("ticket_quantity", 1, 100, $cart_item->ticket_quantity,
+                    {{ Form::selectRangeWithDefault("ticket_quantity", 1, 100, $cart_item->ticket_quantity,
                         ['id' => 'inputGroupSelect01', 'class' => 'custom-select']) }}
                 </div>
             </div>
