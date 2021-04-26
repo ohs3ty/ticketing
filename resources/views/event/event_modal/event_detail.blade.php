affaaff1d43ab5468c8f9281d0182db25382f70d
Caleb <csly2@byu.edu>
2021-04-26 14:00:35 -0600

    test

--- resources/views/event/event_modal/event_detail.blade.php
+++ resources/views/event/event_modal/event_detail.blade.php
@@ -42,10 +42,10 @@
                                     <h5>Ticket Quantity</h5>
                                     @if((now() >= $ticket_count->ticket_open_date) && (now() <= $ticket_count->ticket_close_date))
                                         @if ($ticket_count->ticket_left == null)
-                                            {{ Form::selectRange("ticket_quantity[][$ticket_count->ticket_name]", 0, 100, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
+                                            {{ Form::selectRange("ticket_quantity[][$ticket_count->ticket_type_id]", 0, 100, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
                                         @else
                                         {{ $ticket_count }}
-                                            {{ Form::selectRange("ticket_quantity[][$ticket_count->ticket_name]", 0, $ticket_count->ticket_left, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
+                                            {{ Form::selectRange("ticket_quantity[][$ticket_count->ticket_type_id]", 0, $ticket_count->ticket_left, null, ['class' => 'form-select', 'aria-label' => 'Default select example']) }}
                                         @endif
                                     @else
                                         <span class="text-danger">Tickets not currently selling for this group</span>
