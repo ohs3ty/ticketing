96cdde050e34364a7ca99bf35169146a77e68ec6
Caleb <csly2@byu.edu>
2021-04-26 15:05:33 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -11,8 +11,13 @@ View Cart
         <h2>Your Cart</h2>
         <hr>
         @foreach ($cart_items as $cart_item)
-            <h3>{{ $cart_item->event_name }}</h3>
-            {{ $cart_item }}
+        <div class="row">
+            <div class="col-4">
+                <h3>{{ $cart_item->event_name }}</h3>
+                Ticket Group: {{ $cart_item->ticket_name }}
+            </div>
+        </div>
+        {{ $cart_item }}
         @endforeach
     </div>
     <div class="col-4 bg-primary">
