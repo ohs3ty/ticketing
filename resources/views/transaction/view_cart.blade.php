d60897ef081cb79d5c6d472259f51f07cca299a5
Caleb <csly2@byu.edu>
2021-04-26 16:06:27 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -24,7 +24,7 @@ View Cart
                 </div>
             </div>
             <div class="col-2">
-                <span style="background-color: red;">${{ number_format((floatval($cart_item->ticket_cost) * floatval($cart_item->ticket_quantity)), 2, ".", ",") }}</span>
+                <span style="background-color: red; font-size: 25px;">${{ number_format((floatval($cart_item->ticket_cost) * floatval($cart_item->ticket_quantity)), 2, ".", ",") }}</span>
             </div>
         </div>
         {{ $cart_item }}
