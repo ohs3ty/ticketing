b1dabc068a1f9278f248d52318388ed579eefa1c
Caleb <csly2@byu.edu>
2021-04-26 15:02:04 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -10,7 +10,10 @@ View Cart
     <div class="col-8 bg-danger">
         <h3>Your Cart</h3>
         <hr>
-        {{ $cart_items }}
+        @foreach ($cart_items as $cart_item) {
+            {{ $cart_item->event_name }}
+            {{ $cart_item }}
+        }
     </div>
     <div class="col-4 bg-primary">
     </div>
