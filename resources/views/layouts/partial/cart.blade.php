1654e47c9cbb49d65dd610a22a7e58166286996b
Caleb <csly2@byu.edu>
2021-04-26 14:47:19 -0600

    change id to user_id

--- resources/views/layouts/partial/cart.blade.php
+++ resources/views/layouts/partial/cart.blade.php
@@ -1,4 +1,4 @@
 <div class="text-right" style="margin-right: 10px;">
     <span style="font-size: 1.5rem;"><i class="bi bi-cart"></i></span>
-    <a href="{{ route('mycart', ['id' => Auth::user()->id]) }}">Cart Item(s):</a>
+    <a href="{{ route('mycart', ['user_id' => Auth::user()->id]) }}">Cart Item(s):</a>
 </div>
