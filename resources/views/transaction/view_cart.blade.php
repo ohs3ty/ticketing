6ff53510f138b194eca7aacb8b02a7c38c0f17c9
Caleb <csly2@byu.edu>
2021-04-27 10:07:00 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -56,7 +56,7 @@ View Cart
                 <div class="col-4">${{ number_format($cart_total->ticket_total, 2, ".", ",") }}</div>
             </div>
             <br>
-            <div class="text-center btn">Pay Now</div>
+            <div class="text-center btn btn-primary">Pay Now</div>
         </div>
         <br>
 
