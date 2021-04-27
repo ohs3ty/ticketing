f53bd5344141a36904036c967e02721c342eea32
Caleb <csly2@byu.edu>
2021-04-27 09:31:08 -0600

    test

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -89,6 +89,7 @@ class TransactionController extends Controller {
                                                 AS tc"), 'ticket_types.id', '=', 'tc.id')
                         ->get();
         $cart_item_cost = TempCart::where('user_id', $request->user_id)
+                            ->DB::raw("SELECT temp_carts.id")
                             ->join('ticket_types', 'ticket_types.id', '=', 'temp_carts.ticket_type_id')
                             ->get();
         print($cart_item_cost);
