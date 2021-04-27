ef249338ac25a60a31544fa50265daa91e5c74eb
Caleb <csly2@byu.edu>
2021-04-27 09:55:26 -0600

    test

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -48,7 +48,7 @@ View Cart
         </div>
     </div>
     <div class="col-4">
-        <div style="background-color: rgb(241, 241, 241); padding: 15px;">
+        <div class="shadow-sm rounded">
             <h2>Total</h2>
             <hr>
             <div class="row">
