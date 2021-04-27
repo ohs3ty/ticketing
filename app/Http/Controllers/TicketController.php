63ecec8910a23afb0181ca85b5f1262b2d1cfc34
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-27 17:18:10 -0600

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Event;
+use App\Models\TempCart;
 use App\Models\TicketType;
 
 class TicketController extends Controller
