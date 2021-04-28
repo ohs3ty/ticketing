e3905bc09b098ad585e51e1c2e6421bf63e559ee
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-28 15:01:16 -0600

    Update cart.blade.php

--- resources/views/layouts/partial/cart.blade.php
+++ resources/views/layouts/partial/cart.blade.php
@@ -2,5 +2,7 @@
     <span style="font-size: 1.5rem;"><i class="bi bi-cart"></i></span>
     @if (Auth::check())
         <a href="{{ route('mycart', ['user_id' => Auth::user()->id]) }}">Cart Item(s): </a>
+    @else
+        <a href="{{ route('mycart', ['session_id' => Session::getId()]) }}">Cart Item(s): </a>
     @endif
 </div>
