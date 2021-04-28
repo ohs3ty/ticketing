374fcd1e1f7d383d74555b82efb27787c915470e
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-28 17:12:15 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use App\Models\Transaction;
 
 /*
