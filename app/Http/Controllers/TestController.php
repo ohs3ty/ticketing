49df51387d46ef2a842526ef5121a065285393e4
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-29 09:56:16 -0600

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\TempCart;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function test_buy() {
+    public function test_buy(Request $request) {
+        $cart_items = TempCart::where('user_id', $request->user_id)
+                        ->where('ticket_type_id', $request->ticket_type_id);
+        $cart_total = $cart_items->get();
+
+        dd($cart_total);
+        
+        $new_transaction = new Transaction;
+        
+        
+        foreach($cart_items as $item) {
+            
+
+        }
 
         return ("success");
     }
