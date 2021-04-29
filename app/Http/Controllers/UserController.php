a56c3511187cdaa1f4c496cf70907c8f59102675
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-29 16:28:56 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,6 +19,7 @@ class UserController extends Controller
     public function index(Request $request) {
 
         $user_transactions = Transaction::where('user_id', '=', $request->user_id)
+                                ->join('transaction_tickets', 'transactions.id', '=', 'transaction_tickets.transaction_id' )
                                 ->get();
         return view("user.user_index");
     }
