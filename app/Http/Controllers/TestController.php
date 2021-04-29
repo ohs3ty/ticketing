b3be4d7295742f361bac6200b33aa8a86e9e9de0
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-29 13:04:13 -0600

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,6 @@ use App\Models\TempCart;
 use App\Models\Transaction;
 use App\Models\Customer;
 use App\Models\User;
-use App\Models\TransactionTicket;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
