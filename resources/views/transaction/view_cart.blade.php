2e37bd7971cb7133929eff774c12530d6fa91f1e
ohsha <ohsha@DESKTOP-677NSFV>
2021-04-29 14:35:59 -0600

    Update view_cart.blade.php

--- resources/views/transaction/view_cart.blade.php
+++ resources/views/transaction/view_cart.blade.php
@@ -73,7 +73,7 @@ View Cart
 
 <br><br>
     {{ hash('sha256', date('Ymd')) }}<br>
-    {{ substr(hash('sha256', date('Ymd')), 0, 5) }}<br>
+    {{ substr(hash('sha256', date('Ymd')), 0, 5) + substr(time(), 0, 5) }}<br>
     {{ time() }}
     {{-- for testing --}}
     @if (Auth::user())
