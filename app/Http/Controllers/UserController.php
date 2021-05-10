9cb7f3fe063eb7e43963d0cd12fee64ef1ead28b
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-10 15:59:03 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 {
     public function index(Request $request) {
         $user_transactions = Transaction::where('user_id', $request->user_id)
-                                ->select('transactions.id as transaction_id', 'transaction_total', 'transactions.created_at as created_at')
+                                ->select('transactions.id as transaction_id', 'transaction_total', 'transactions.created_at as transaction_date')
                                 ->join('customers', 'customers.id', '=', 'transactions.customer_id')
                                 ->get();
                                 // ->join('transaction_tickets', 'transactions.id', '=', 'transaction_tickets.transaction_id' )
