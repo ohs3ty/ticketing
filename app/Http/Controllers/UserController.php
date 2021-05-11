ea265aa31f77ad1a23a2c28c9200aed5488d9f6b
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-11 14:20:33 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,8 +26,7 @@ class UserController extends Controller
                                 // ->join('transaction_tickets', 'transactions.id', '=', 'transaction_tickets.transaction_id' )
 
         $transaction_details = TransactionTicket::all()
-                                ->join('ticket_types', 'ticket_types.id', '=', 'transaction_tickets.ticket_type_id')
-                                ->get();
+                                ->join('ticket_types', 'ticket_types.id', '=', 'transaction_tickets.ticket_type_id');
         return view("user.user_index", [
             'user_transactions' => $user_transactions,
             'transaction_details' => $transaction_details,
