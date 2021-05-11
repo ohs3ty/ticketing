c18ce14609e32ac17b665583e2356c6f14442105
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-11 15:59:31 -0600

    Update user_index.blade.php

--- resources/views/user/user_index.blade.php
+++ resources/views/user/user_index.blade.php
@@ -5,34 +5,38 @@ My Tickets and Orders
 @endsection
 
 @section('content')
-@if(Auth::user()->id == $user_id)
-    <h3>Past Orders</h3>
+
+@auth
+    @if(Auth::user()->id == $user_id)
+        <h3>Past Orders</h3>
 
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Date Purchased</th>
-                <th>Transaction Number</th>
-                <th>Transaction Total</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($user_transactions as $transaction)
-                <tr onclick="transactionDetails()">
-                    <td>{{ \Carbon\Carbon::parse($transaction->transaction_date)->format("F j, Y g:i a") }}</td>
-                    {{-- modal --}}
-                    <td>    
-                        <a href="{{ route('transaction_details', ['transaction_id' => $transaction->transaction_id]) }}">
-                            {{ $transaction->transaction_id }}
-                        </a>
-                    </td>
-                    <td>${{ number_format($transaction->transaction_total, 2, ".", ",") }}</td>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Date Purchased</th>
+                    <th>Transaction Number</th>
+                    <th>Transaction Total</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endif
+            </thead>
+            <tbody>
+                @foreach($user_transactions as $transaction)
+                    <tr onclick="transactionDetails()">
+                        <td>{{ \Carbon\Carbon::parse($transaction->transaction_date)->format("F j, Y g:i a") }}</td>
+                        {{-- modal --}}
+                        <td>    
+                            <a href="{{ route('transaction_details', ['transaction_id' => $transaction->transaction_id]) }}">
+                                {{ $transaction->transaction_id }}
+                            </a>
+                        </td>
+                        <td>${{ number_format($transaction->transaction_total, 2, ".", ",") }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+@endauth
+
 
 
 @endsection
