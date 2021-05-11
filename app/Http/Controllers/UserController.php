ef93fbc27dfe5f47ef5b220ebc0ada4bdb5fddd4
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-11 15:51:06 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
         ]);
     }
 
-    public function transaction_detail(Request $request) {
+    public function transaction_details(Request $request) {
         $transaction_details = TransactionTicket::select('transaction_tickets.transaction_id', 'events.event_name', 'quantity', 'ticket_cost', 'events.start_date')
                                 ->join('ticket_types', 'ticket_types.id', '=', 'transaction_tickets.ticket_type_id')
                                 ->join('events', 'events.id', '=', 'ticket_types.event_id')
