717a93ef554a4bd3680e7dac85b0e082d0266eeb
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-12 14:38:49 -0600

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -209,13 +209,13 @@ class AdminController extends Controller
         if ($request->deleteorganizer == "true") {
             $organizer_organization = OrganizationOrganizer::where('organizer_id', $request->organizer_id)->get();
             foreach($organizer_organization as $org) {
-                $org->delete();
+                // $org->delete();
             }
             
             $organizer = Organizer::where('id', $request->organizer_id)->first();
             $user = User::where('id', $organizer->user_id)->first();
             $user->resetRole();
-            $organizer->delete();
+            // $organizer->delete();
 
 
             return back();
