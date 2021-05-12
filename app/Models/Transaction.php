c8faed5d8d248f57eae91971d4194a90d5f9ee83
ohsha <ohsha@DESKTOP-677NSFV>
2021-05-12 08:51:07 -0600

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\NameOrderScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
